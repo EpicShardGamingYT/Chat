e7d340503578b5ba78bce781f96a76d3274fa55e
EpicShardGaming <surtla100@gmail.com>
2018-07-20 17:45:33 +0200

    just pushing for collabs

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import *
+import os
+import time
+from datetime import datetime
+import requests
+import json
+import base64
+import random
+import lib
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+
+
+
+@app.route('/lib/send/<string:message>/<int:author>')
+def send_message(message,author):
+	lib.database.new_message(message,author)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))
